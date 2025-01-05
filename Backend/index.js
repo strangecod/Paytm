47220fa0e4e199ed8632fec663478958c9a4bedf
priyankakashyap123 <priyanka@codemaya.com>
2025-01-05 14:54:06 +0530

    P-1 #comment Add the index,js file

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log("Server Started new")
+})
